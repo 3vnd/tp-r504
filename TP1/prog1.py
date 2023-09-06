76aa6f66ce1b1849a74bf0acc6f68f7b86a2ef86
voydieva <evan.voydis@univ-rouen.fr>
2023-09-06 17:08:41 +0200

    update py

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,16 +1,6 @@
 import fonctions as f
-
-a = int(input("Entrez a : "))
-b = int(input("Entrez b : "))
-f.puissance(a,b)
-
-
-
-"""
-nombre = int(input("Entrez un nombre : "))
-carre = nombre **2
-count = 0 
-for count in range (carre):
-	print("Hello world!")
-	count+=1
-"""
+while True:
+	a = int(input("Entrez a : "))
+	b = int(input("Entrez b : "))
+	c = f.puissance(a,b)
+	print(c)
