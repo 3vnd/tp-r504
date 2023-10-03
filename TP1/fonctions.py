cd7d30f65952b0fa50f12cca46fa39995e28673e
voydieva <evan.voydis@univ-rouen.fr>
2023-10-03 09:00:05 +0200

    maj

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,2 +1,9 @@
-def puissance (a,b):
+def puiss (a,b):
+	if not type(a) is int:
+		raise TypeError("Only integers are allowed")
+	if not type(b) is int:
+		raise TypeError("Only integers are allowed")
+	if a==0 and b<0:
+		raise TypeError("0 ne peut pas avoir une puissance négative")
+	else:
 		return a**b
