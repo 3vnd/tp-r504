d4bca3f0c0413012bbb51cc886cfe1345f2fe26b
voydieva <evan.voydis@univ-rouen.fr>
2024-01-25 16:53:46 +0100

    java

--- TP9/ClientTCP1.java
+++ TP9/ClientTCP1.java
@@ -1,8 +1,14 @@
 import java.io.*;
 import java.net.*;
 
-//ClientTCP.java
-Socket socket = new Socket( "LocalHost", 2016 );
-DataOutputStream dOut = DataOutputStream( socket.getOutputStream() );
-dOut.writeUTF( "message test" );
-socket.close();
+public class ClientTCP1
+{
+	public static void main( String[] args )
+	{
+		//ClientTCP.java
+		Socket socket = new Socket( "LocalHost", 2016 );
+		DataOutputStream dOut = DataOutputStream( socket.getOutputStream() );
+		dOut.writeUTF( "message test" );
+		socket.close();
+	}
+}
--- TP9/ServeurTCP1.java
+++ TP9/ServeurTCP1.java
@@ -1,17 +1,18 @@
-public class TP9
+import java.io.*;
+import java.net.*;
+
+public class ServeurTCP1
 {
 	public static void main( String[] args )
 	{
 		try {
-				import java.io.*;
-				import java.net.*;
 				
 				// serveurTCP1.java 
 				ServerSocket socketserver = new ServerSocket( 2016 );
 				System.out.println( "serveur en attente" );
-				Socket socket = sockserver.accept();
+				Socket socket = socketserver.accept();
 				System.out.println( "Connection d'un client" );
-				DataInputStream dIn = new dataInputStream( socket.getInputStream() );
+				DataInputStream dIn = new DataInputStream( socket.getInputStream() );
 				System.out.println( "Message: " + dIn.readUTF() );
 				
 				socket.close();
